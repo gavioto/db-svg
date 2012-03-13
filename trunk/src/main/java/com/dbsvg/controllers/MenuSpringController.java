cd93fcb50252d1b9ab6877bc2310679a6c867a8d
derrick.bowen <derrick.bowen@43dbb56c-25e6-11de-8b5c-13accb87f508>
2012-03-13 15:26:53 +0000

    added controller

--- trunk/src/main/java/com/dbsvg/controllers/MenuSpringController.java
+++ trunk/src/main/java/com/dbsvg/controllers/MenuSpringController.java
@@ -4,14 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Handles requests for the application home page.
+ * Handles requests for the application menu.
  */
 @Controller
 public class MenuSpringController {
 
-	@RequestMapping(value = "/menu")
-	public String home() {
-		return "menu.jsp";
+	@RequestMapping(value = "/")
+	public String menu() {
+		return "home.jsp";
 	}
 
 }
